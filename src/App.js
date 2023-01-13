2b913b26477d8447e28a52291ea3ece348c4b1cb
Nilesh <nilesh.dangi@wowlabz.com>
2023-01-13 19:16:52 +0530

    Bedtime stories book

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ import UploadIcon from './assets/upload.png'
 
 const storage = global.localStorage || null
 
-const DEMO_URL = '/files/english.epub'
-const DEMO_NAME = 'Gold stars english'
+const DEMO_URL = '/files/bedtime-stories.epub'
+const DEMO_NAME = 'Bedtime stories'
 
 const GlobalStyle = createGlobalStyle`
   * {
